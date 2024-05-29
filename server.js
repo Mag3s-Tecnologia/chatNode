3c1fcea6901fdc0021fd0d8fe9c314e69847b21e
André Freire <andrefreirecg@gmail.com>
2024-05-29 12:53:28 -0300

    support_RoomId

--- server.js
+++ server.js
@@ -14,14 +14,22 @@ app.use('/', (req, res) => {
   res.render('index.html')
 })
 
-let messages = []
+let chatRooms = {}
 
 io.on('connection', socket => {
-    socket.emit('previousMessages', messages)
-    socket.on('sendMessage', data => {
-        messages.push(data)
-        socket.broadcast.emit('receivedMessage', data)
-    })
+  socket.on('joinRoom', ({roomId}) => {
+      socket.join(roomId)
+      if (!chatRooms[roomId]) {
+          chatRooms[roomId] = []
+      }
+      socket.emit('previousMessages', chatRooms[roomId])
+  })
+
+  socket.on('sendMessage', ({roomId, username, message}) => {
+      let messageObj = {username: username, message: message}
+      chatRooms[roomId].push(messageObj)
+      socket.to(roomId).emit('receivedMessage', messageObj)
+  })
 })
 
 server.listen(3000)
