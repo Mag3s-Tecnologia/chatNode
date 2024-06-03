69e029800188842d20e32c3b97878f6b60ffcb21
André Freire <andrefreirecg@gmail.com>
2024-06-03 14:58:26 -0300

    update_files_content

---
+++ functions/store.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const path = require('path');
+const moment = require('moment');
+
+function storeChatMessage(roomId, message) {
+  const chatMessages = getChatMessages(roomId);
+  const lastMessage = chatMessages[chatMessages.length - 1];
+  message.time = new Date();
+  let approval = false;
+  if (lastMessage && lastMessage.username === message.username) {
+      const format = 'h:mm:ss A';
+      const lastMessageTime = moment(lastMessage.time, format).valueOf();
+      const messageTime = moment(message.time, format).valueOf();
+
+      // Verificar se mandaram a mensagem a mais de 5 segundos
+      if (messageTime - lastMessageTime < 5000) {
+        approval = false;
+        return {
+          'status': 'error',
+          'message': 'Você está enviando mensagens muito rápido! Espere 5 segundos para enviar outra mensagem.'
+        }
+      }else{
+        approval = true;
+        message.time = new Date().toLocaleTimeString();
+      }
+  }else{
+    approval = true;
+    message.time = new Date().toLocaleTimeString();
+  }
+  if (approval) {
+    chatMessages.push(message);
+    const date = new Date().toISOString().slice(0,10); // get current date in YYYY-MM-DD format
+    const dir = path.join(__dirname, '../messages');
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    fs.writeFileSync(`${dir}/messages-${roomId}-${date}.json`, JSON.stringify(chatMessages));
+    return {
+      'status': 'success',
+      'message': 'Mensagem enviada com sucesso!'
+    }
+  }
+}
+function getChatMessages(roomId) {
+  const date = new Date().toISOString().slice(0,10); // get current date in YYYY-MM-DD format
+  const dir = path.join(__dirname, '../messages');
+  const filePath = `${dir}/messages-${roomId}-${date}.json`;
+  if (fs.existsSync(filePath)) {
+    const data = JSON.parse(fs.readFileSync(filePath));
+    const filteredData = data.filter(message => message.message !== '').slice(-50);
+
+    return filteredData
+  } else {
+    const dataPrimal = [
+      {
+        username: 'Chatbot',
+        message: 'Bem-vindo ao chat! 🎉',
+        time: new Date().toLocaleTimeString()
+      }
+    ];
+    fs.writeFileSync(`${dir}/messages-${roomId}-${date}.json`, JSON.stringify(dataPrimal));
+    const data = JSON.parse(fs.readFileSync(`${dir}/messages-${roomId}-${date}.json`));
+    const filteredData = data.filter(message => message.message !== '').slice(-50);
+    return filteredData;
+  }
+}
+module.exports = {
+  getChatMessages,
+  storeChatMessage
+};
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const functions = require('./functions/store.js');
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'public'))
@@ -13,23 +13,21 @@ app.set('view engine', 'html')
 app.use('/', (req, res) => {
   res.render('index.html')
 })
-
-let chatRooms = {}
-
 io.on('connection', socket => {
   socket.on('joinRoom', ({roomId}) => {
       socket.join(roomId)
-      if (!chatRooms[roomId]) {
-          chatRooms[roomId] = []
-      }
-      socket.emit('previousMessages', chatRooms[roomId])
+      let chatMessages = functions.getChatMessages(roomId);
+      io.in(roomId).emit('previousMessages', chatMessages)
   })
 
   socket.on('sendMessage', ({roomId, username, message}) => {
-      let messageObj = {username: username, message: message}
-      chatRooms[roomId].push(messageObj)
-      socket.to(roomId).emit('receivedMessage', messageObj)
+    let messageObj = {username: username, message: message}
+    const data = functions.storeChatMessage(roomId, messageObj)
+    if (data.status == 'success') {
+      io.in(roomId).emit('receivedMessage', messageObj)
+    }else{
+      socket.emit('error', data.message)
+    }
   })
 })
-
-server.listen(3000)
+server.listen(3001)
